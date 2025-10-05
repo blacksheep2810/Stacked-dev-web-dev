f1106e3ed3391280c18c70d3924bd79622c9163f
blacksheep2810 <g22c7386@campus.ru.ac.za>
2025-10-05 19:48:40 +0200

    added prac 4 folder with Hero video on home page

---
+++ Prac 4/README.md
@@ -0,0 +1,4 @@
+A repository for our 2025 CS301 WebDev project. 
+
+Co-collaborators: Kopano Lesese, Thomas Coutts, Hazel Chagaluka, Reitumetse Malope.
+Date of creation: September 2025
---
+++ Prac 4/javascript/Culture.js
@@ -0,0 +1,30 @@
+let slideIndex = [1, 1, 1, 1, 1];
+
+let slideID = ["SlidesC", "SlidesF", "SlidesM", "SlidesG", "SlidesD"]
+
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
+showSlides(1, 3);
+showSlides(1, 4);
+
+
+/*change slides upon click*/
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+
+function showSlides(n, no) {
+  let i;
+  let slide = document.getElementsByClassName(slideID[no]);
+  if (n > slide.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = slide.length}
+  for (i = 0; i < slide.length; i++) {
+    slide[i].style.display = "none";
+  }
+
+
+  slide[slideIndex[no]-1].style.display = "block";
+} 
+/* heavily reference this to assist: https://www.w3schools.com/howto/howto_js_slideshow.asp*/
